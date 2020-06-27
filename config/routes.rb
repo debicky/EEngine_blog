6656e4cb0044a7fc2ec5ff0d0aec24953efaf895
debicky <arturd444@gmail.com>
2020-06-27 09:36:18 +0200

    navbar functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :authors
   root 'home#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Author.find_by_email('admin@admin.com').nil?
+  author = Author.new(
+    email: "admin@admin.com",
+    first_name: "FirstName",
+    last_name: "LastName",
+    password: "password",
+    password_confirmation: "password"
+  )
+  author.save
+end
+
+
+10.times do |i|
+  Author.create!(first_name: Faker::Name.unique.first_name,
+               last_name: Faker::Name.unique.last_name,
+               email: Faker::Internet.unique.email,
+               password: 'password',
+               password_confirmation: 'password'
+               )
+end
